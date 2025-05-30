d1d4698858e9b928e55797b6c178b982bbb1a5a2
ishwor <ishwornepal1234@gmail.com>
2025-05-30 17:37:36 +0545

    commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index() {
+        return response()->json(User::all());
+    }
+
+    public function store(Request $request) {
+        $user = User::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    public function show($id) {
+        $user = User::findOrFail($id);
+        return response()->json($user);
+    }
+
+    public function update(Request $request, $id) {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
+    public function destroy($id) {
+        User::destroy($id);
+        return response()->json(null, 204);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\IPController;
+use App\Http\Controllers\Api\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+# All Authentication Routes
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    route::post('/register', [AuthController::class, 'storeUser']);
+});
+
+# All services api routes
+Route::group(['middleware' => ['ip-whitelist','api','jwt-verify'],'prefix' => 'v1'], function ($router) {
+
+    Route::apiResource('roles', RoleController::class);
+    Route::apiResource('ips', IPController::class);
+    
+});
+
+
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
